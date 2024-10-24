80b2c911bc97c2126675b6cb702ea7e1a60c62db
jadon <16850875+wolffshots@users.noreply.github.com>
2024-10-24 20:07:58 +0200

    chore: added tracing and better handling in entrypoint and renamed vars

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@
 FROM alpine:latest AS builder
 
 # Install iproute2 and ip
-RUN apk --no-cache add iproute2 iputils
+RUN apk --no-cache add iproute2 iputils traceroute
 RUN rm -rf /var/cache/apk/*
 
 # Second stage: Minimal runtime image (BusyBox)
 FROM busybox:latest
 
 COPY --from=builder /sbin/ip /sbin/ip
+COPY --from=builder /usr/bin/traceroute /usr/bin/traceroute
 
 # Copy entrypoint script
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 ip route del default
 
 # Add local subnets via BYPASS_IP
-for subnet in ${LOCAL_SUBNETS//,/ }
+for subnet in ${BYPASS_SUBNETS//,/ }
 do
     echo "Adding route for subnet $subnet via $BYPASS_IP"
     ip route add $subnet via $BYPASS_IP
@@ -20,19 +20,48 @@ ip route
 
 sleep 2
 
+if [ "${TRACE}" = "on" ] || [ "${TRACE}" = "true" ] || [ "${TRACE}" = "ON" ] || [ "${TRACE}" = "TRUE" ]; then
+    echo "Running traceroute..."
+    traceroute "$HEALTH_REMOTE_IP"
+    traceroute "$HEALTH_LOCAL_IP"
+fi
+
 echo "IP through the default route:"
-echo $(busybox wget -qO- ifconfig.me/ip || "Failed to fetch IP")
+echo $(busybox wget -qO- $IP_API_URL || "Failed to fetch IP")
+
+check_health() {
+    local ip="$1"
+    if ! ping -c 1 "$ip" >/dev/null 2>&1; then
+        echo "Health check failed: Cannot ping $ip"
+    fi
+}
+
+# Total duration between health checks in seconds
+TOTAL_SLEEP=30
+# Shorter sleep duration in seconds
+SHORT_SLEEP=1
+
+# Function to handle cleanup on SIGTERM
+cleanup() {
+    echo "Shutting down gracefully..."
+    exit 0
+}
+
+# Trap SIGTERM and SIGINT signals
+trap cleanup SIGTERM SIGINT
 
 # Infinite loop to keep container running
 while true; do
     # Health check pings
-    if ! ping -c 1 $INTERNET_IP >/dev/null 2>&1; then
-        echo "Health check failed: Cannot ping INTERNET_IP ($INTERNET_IP)"
-    fi
+    check_health "$HEALTH_REMOTE_IP"
+    check_health "$HEALTH_LOCAL_IP"
 
-    if ! ping -c 1 $LOCAL_IP >/dev/null 2>&1; then
-        echo "Health check failed: Cannot ping LOCAL_IP ($LOCAL_IP)"
-    fi
+    # Initialize elapsed time
+    elapsed=0
 
-    sleep 30
+    # Sleep in shorter intervals until total sleep is reached
+    while [ "$elapsed" -lt "$TOTAL_SLEEP" ]; do
+        sleep "$SHORT_SLEEP"
+        elapsed=$((elapsed + SHORT_SLEEP))
+    done
 done
